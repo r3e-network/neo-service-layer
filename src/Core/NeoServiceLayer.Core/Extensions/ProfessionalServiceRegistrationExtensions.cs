88f09ada3c57860a90c486f528c02803a505f6a7
jimmy <jimmy@r3e.network>
2025-08-21 08:07:09 +0000

    🔖 Checkpoint: Edit /home/ubuntu/neo-service-layer/src/Core/NeoServiceLayer.Core/Extensions/ProfessionalServiceRegistrationExtensions.cs

--- src/Core/NeoServiceLayer.Core/Extensions/ProfessionalServiceRegistrationExtensions.cs
+++ src/Core/NeoServiceLayer.Core/Extensions/ProfessionalServiceRegistrationExtensions.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using NeoServiceLayer.Core.Attributes;
+using NeoServiceLayer.Core.Configuration;
 using NeoServiceLayer.Core.Domain.Handlers;
 using NeoServiceLayer.Core.Health;
 
