993a8e30a2011202928465f78077703aa1a204dc
jimmy <jimmy@r3e.network>
2025-08-22 04:17:39 +0000

    🔖 Checkpoint: Edit /home/ubuntu/neo-service-layer/src/Core/NeoServiceLayer.Shared/Extensions/StringExtensions.cs

--- src/Core/NeoServiceLayer.Shared/Extensions/StringExtensions.cs
+++ src/Core/NeoServiceLayer.Shared/Extensions/StringExtensions.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
