f98508674bc01f163308392879fa5738d2cf48b2
jimmy <jimmy@r3e.network>
2025-08-22 05:44:32 +0000

    🔖 Checkpoint: Edit /home/ubuntu/neo-service-layer/src/Infrastructure/NeoServiceLayer.Infrastructure.Persistence/PostgreSQL/Repositories/PostgreSQLSealedDataRepository.cs

--- src/Infrastructure/NeoServiceLayer.Infrastructure.Persistence/PostgreSQL/Repositories/PostgreSQLSealedDataRepository.cs
+++ src/Infrastructure/NeoServiceLayer.Infrastructure.Persistence/PostgreSQL/Repositories/PostgreSQLSealedDataRepository.cs
@@ -5,7 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using NeoServiceLayer.Services.EnclaveStorage.Models;
+using NeoServiceLayer.ServiceFramework.Models;
 
 namespace NeoServiceLayer.Infrastructure.Persistence.PostgreSQL;
 
