20fd04d8606f6f4191618f978aeb891a63f3e0a0
jimmy <jimmy@r3e.network>
2025-08-22 03:28:38 +0000

    🔖 Checkpoint: Edit /home/ubuntu/neo-service-layer/src/Core/NeoServiceLayer.Core/ConfidentialComputing/ConfidentialComputingService.cs

--- src/Core/NeoServiceLayer.Core/ConfidentialComputing/ConfidentialComputingService.cs
+++ src/Core/NeoServiceLayer.Core/ConfidentialComputing/ConfidentialComputingService.cs
@@ -5,7 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NeoServiceLayer.Core.Domain;
-using NeoServiceLayer.Tee.Enclave;
+// using NeoServiceLayer.Tee.Enclave; // TODO: Add project reference when circular dependency resolved
 
 namespace NeoServiceLayer.Core.ConfidentialComputing
 {
