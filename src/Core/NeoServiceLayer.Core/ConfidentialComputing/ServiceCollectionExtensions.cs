d65f6efba404b0be4a60eddb75bd5edee06f708b
jimmy <jimmy@r3e.network>
2025-08-23 00:01:42 +0000

    🔖 Checkpoint: Edit /home/ubuntu/neo-service-layer/src/Core/NeoServiceLayer.Core/ConfidentialComputing/ServiceCollectionExtensions.cs

--- src/Core/NeoServiceLayer.Core/ConfidentialComputing/ServiceCollectionExtensions.cs
+++ src/Core/NeoServiceLayer.Core/ConfidentialComputing/ServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NeoServiceLayer.Core.Domain;
-using NeoServiceLayer.Tee.Enclave;
+// Note: TEE namespace handled via service collection registration
 using System;
 
 namespace NeoServiceLayer.Core.ConfidentialComputing
